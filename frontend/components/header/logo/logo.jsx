eeb671af737e67a20c54d7cfe3a63f2401a79c61
Matt <matthewlockewong@gmail.com>
2017-11-05 21:30:22 -0800

    changed logo

--- frontend/components/header/logo/logo.jsx
+++ frontend/components/header/logo/logo.jsx
@@ -7,7 +7,7 @@ const Logo = () => (
       <Link to="/" className="logo-anchor">
         <div className="logo-div-3">
           <div className="logo-div-4">
-            <img id="logo" src='http://res.cloudinary.com/mwong9968/image/upload/v1507011825/logo_nutgug.jpg'></img>
+            <img id="logo" src='http://res.cloudinary.com/mwong9968/image/upload/v1509946098/Screen_Shot_2017-11-05_at_21.27.59_xqtv9p.jpg'></img>
           </div>
         </div>
       </Link>
